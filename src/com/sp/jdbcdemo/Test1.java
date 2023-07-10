20889377aa57509d04d351b4ccb0377a56884c44
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2023-07-11 00:04:28 +0530

    Add files via upload

---
+++ src/com/sp/jdbcdemo/Test1.java
@@ -0,0 +1,46 @@
+package com.sp.jdbcdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Test1 
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			
+			// 1. load the driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//2 Create database connection
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_demo_db", "root", "root");
+			
+			//3. Create Statement
+			Statement st=con.createStatement();
+			
+			//4. get resultset
+			ResultSet rs=st.executeQuery("select * from register");
+			
+			//5. get values from resultset
+			while(rs.next())
+			{
+				String name1=rs.getString("name");
+				String email1=rs.getString("email");
+				String gender1=rs.getString("gender");
+				String city1=rs.getString("city");
+				
+				System.out.println(name1+" - "+email1+" - "+gender1+" - "+city1);
+			}
+			
+			rs.close();
+			st.close();
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/sp/jdbcdemo/Test2.java
@@ -0,0 +1,56 @@
+package com.sp.jdbcdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class Test2 
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("Enter Name");
+		String name1=s.next();
+		
+		System.out.println("Enter Email");
+		String email1=s.next();
+		
+		System.out.println("Enter Password");
+		String pass1=s.next();
+		
+		System.out.println("Enter Gender");
+		String gender1=s.next();
+		
+		System.out.println("Enter City");
+		String city1=s.next();
+		
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_demo_db", "root", "root");
+			
+			PreparedStatement ps=con.prepareStatement("insert into register values(?,?,?,?,?)");
+			ps.setString(1, name1);
+			ps.setString(2, email1);
+			ps.setString(3, pass1);
+			ps.setString(4, gender1);
+			ps.setString(5, city1);
+			
+			int i=ps.executeUpdate();
+			if(i>0)
+			{
+				System.out.println("success");
+			}
+			else
+			{
+				System.out.println("fail");
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
